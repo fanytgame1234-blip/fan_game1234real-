a2e4d2143a6450f3c4aa611cc4e863dab0653f58
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-08-24 21:10:37 +0700

    Update Fix-lag.lua

--- Fix-lag.lua
+++ Fix-lag.lua
@@ -1,18 +1,4 @@
--- Giảm lag cực mạnh, chỉ giữ lại player + quái + collider map
---local urls = {
-   -- "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_sky.lua",
-  --  "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_lightning.lua",
-  --  "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_models.lua", 
-   -- "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_texture_player-mob.lua", 
-   -- "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_animation.lua", 
-   -- "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_other_sounds.lua",  
-  --  "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_player_sounds.lua"
-    
---}
-
---for i, url in ipairs(urls) do
-   -- task.spawn(function()
-      --  task.wait((i - 1) * 5) -- chờ 0s, 10s, 20s theo 
+-- Giảm lag cực mạnh
             
             
             
@@ -52,6 +38,7 @@ for i, url in ipairs(urls) do
         end)
     end)
 end
+
 
 
 
