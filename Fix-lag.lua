52acaef1ec3860bb5a3b8d0e11dc58dc7dcdc06c
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-08-24 15:34:30 +0700

    Fix-lag.lua

--- Fix-lag.lua
+++ Fix-lag.lua
@@ -3,7 +3,7 @@ local urls = {
     "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_sky.lua",
     "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_lightning.lua",
     "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_models.lua", 
-    " https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_texture_player-mob.lua"
+    "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/refs/heads/main/delete_texture_player-mob.lua"
 }
 
 for i, url in ipairs(urls) do
@@ -19,6 +19,7 @@ for i, url in ipairs(urls) do
         end
     end)
 end
+
 
 
 
