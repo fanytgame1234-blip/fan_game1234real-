54e35e18b5210cb2c10b801df7d3617f873ab3f2
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-08-25 15:53:24 +0700

    Update Fix-lag.lua

--- Fix-lag.lua
+++ Fix-lag.lua
@@ -17,7 +17,9 @@ local scripts = {
     [86076978383613] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Hunty.lua",
 
     -- Grow a Garden
-    [126884695634066] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Gag.lua"
+    [126884695634066] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Gag.lua", 
+    -- Build a plane 
+    [137925884276740] = "https://raw.githubusercontent.com/fanytgame1234-blip/1234/main/Fix-Lag-Build-A-Plane.lua"
 }
 
 -- Kiểm tra và chạy script con
@@ -39,6 +41,7 @@ if url then
 else
     warn("⛔ No Fix-Lag script for this PlaceId:", placeId)
     end                       
+
 
 
 
