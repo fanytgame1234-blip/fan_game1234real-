01d1da841b3bacc2d7d188c1219c9eebbc5c7de1
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-08-28 20:41:33 +0700

    Update Fix-Lag-Gag.lua

--- Fix-Lag-Gag.lua
+++ Fix-Lag-Gag.lua
@@ -7,9 +7,10 @@ local gagUrls = {
     "https://raw.githubusercontent.com/fanytgame1234-blip/1234/main/delete_fx_gag.lua", 
     "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/delete_all_sounds_Gag.lua", 
     "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/delete_animation_Gag.lua", 
-    "https://raw.githubusercontent.com/fanytgame1234-blip/1234/main/delete_model_plantgag.lua"
+    "https://raw.githubusercontent.com/fanytgame1234-blip/1234/main/delete_model_plantgag.lua", 
+    "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/hide_other_player.lua", 
+    "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/refs/heads/main/lowui_.lua"
 }
-
 for i, url in ipairs(gagUrls) do
     task.wait(1 * (i - 1))
     pcall(function()
@@ -17,6 +18,7 @@ for i, url in ipairs(gagUrls) do
     end)
 
 end
+
 
 
 
