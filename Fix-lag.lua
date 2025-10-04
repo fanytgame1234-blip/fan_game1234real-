58da7a342686ada9d01470690fe7f0064ec31920
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-10-04 17:37:29 +0700

    Update Fix-lag.lua

--- Fix-lag.lua
+++ Fix-lag.lua
@@ -6,26 +6,7 @@ local StarterGui = game:GetService("StarterGui")
 local player = Players.LocalPlayer
 local placeId = game.PlaceId
 
--- Bảng PlaceId -> URL file con
-local scripts = {
-    -- Blox Fruits
-    [2753915549] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Blox.lua",
-    [4442272183] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Blox.lua",
-    [7449423635] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Blox.lua",
-
-    -- Hunty Zombie
-    [86076978383613] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Hunty.lua",
-
-    -- Grow a Garden
-    [126884695634066] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Gag.lua", 
-    -- Build a plane    
-    [137925884276740] = 
-    "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/refs/heads/main/Main-Fix-Lag-Build-A-Plane.lua", 
-    --Dead rails 
-    [70876832253163] = "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/Main-Fix-Lag_Dead-Rails.lua"
-}
-
--- Ki      
+--
 
 
 -- Script Fix Lag By @fan_game1234 (Y-T)
@@ -83,6 +64,7 @@ else
         Duration = 5
     })
 end
+
 
 
 
