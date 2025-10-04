a83766e296ae6677dbdbd888bc073f58806e2e05
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-10-04 17:36:42 +0700

    Update Fix-lag.lua

--- Fix-lag.lua
+++ Fix-lag.lua
@@ -25,6 +25,37 @@ local scripts = {
     [70876832253163] = "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/Main-Fix-Lag_Dead-Rails.lua"
 }
 
+-- Ki      
+
+
+-- Script Fix Lag By @fan_game1234 (Y-T)
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
+local player = Players.LocalPlayer
+local placeId = game.PlaceId
+
+-- Bảng PlaceId -> URL file con
+local scripts = {
+    -- Blox Fruits
+    [2753915549] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Blox.lua",
+    [4442272183] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Blox.lua",
+    [7449423635] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Blox.lua",
+
+    -- Hunty Zombie
+    [86076978383613] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Hunty.lua",
+
+    -- Grow a Garden
+    [126884695634066] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Gag.lua", 
+
+    -- Build a plane    
+    [137925884276740] = "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/refs/heads/main/Main-Fix-Lag-Build-A-Plane.lua", 
+
+    -- Dead rails 
+    [70876832253163] = "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/Main-Fix-Lag_Dead-Rails.lua"
+}
+
 -- Kiểm tra và chạy script con
 local url = scripts[placeId]
 if url then
@@ -39,19 +70,20 @@ if url then
             Duration = 5
         })
     else
-        StarterGui:Setcore(" SendNotification", { 
-                Title = "Fix lag"  
-                Text = "Tải không được!:(", 
-                Duration = 5 
-            }) 
+        StarterGui:SetCore("SendNotification", { 
+            Title = "Fix Lag",
+            Text = "Tải không được! :(",
+            Duration = 5
+        })
     end
 else
-    StarterGui:Setcore("SendNotificatin", { 
-            Title = " Fix lag", 
-            Text = "💀Lỗi rồi!", 
-            Duration = 5
-        }) 
-    end                       
+    StarterGui:SetCore("SendNotification", { 
+        Title = "Fix Lag",
+        Text = "💀 Lỗi rồi!",
+        Duration = 5
+    })
+end
+
 
 
 
