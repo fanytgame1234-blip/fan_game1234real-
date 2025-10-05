da3849968efb9fd50875c4c475e09cc86f901d3d
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-10-05 16:47:55 +0700

    Update Main- fix-lag-PvB.lua

--- Main- fix-lag-PvB.lua
+++ Main- fix-lag-PvB.lua
@@ -4,8 +4,9 @@ local Pvburl = {
     "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/hide_other_player.lua", 
     "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/delete_lightning_gag.lua", -- dùng lại của Gag vì tui hơi lười:) 
     "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/hide_model_plant_vs_brainrot.lua"
+    "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model//main/delete_sky.lua", 
+    "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/delete_all_sound_all_game.lua", 
 }
-
 for i, url in ipairs(Pvburl) do
     task.wait(1 * (i - 1))
     pcall(function()
@@ -13,3 +14,4 @@ for i, url in ipairs(Pvburl) do
     end)
 
 end   
+
