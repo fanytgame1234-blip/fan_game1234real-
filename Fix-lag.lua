f53adcddaf05b9b451c46b4f3edb5e31d9c0adc1
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-10-05 13:07:36 +0700

    Update Fix-lag.lua

--- Fix-lag.lua
+++ Fix-lag.lua
@@ -1,26 +1,5 @@
  -- Script Fix Lag By @fan_game1234 (Y-T)
-if not game:IsLoaded() then game.Loaded:Wait() end
-
-local Players = game:GetService("Players")
-local StarterGui = game:GetService("StarterGui")
-local player = Players.LocalPlayer
-local placeId = game.PlaceId
-
--- Bảng PlaceId -> URL file con
-local scripts = {
-    -- Blox Fruits
-    [2753915549] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Blox.lua",
-    [4442272183] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Blox.lua",
-    [7449423635] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Blox.lua",
-
-    -- Hunty Zombie
-    [86076978383613] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Hunty.lua",
-
-    -- Grow a Garden
-    [126884695634066] = "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Gag.lua", 
-
-    -- Build a plane    
-    [1379258
+if not ga
 
 
 
