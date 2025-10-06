42614bdcb30da5abe27a7e688bdc25001b355a96
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-10-06 21:11:00 +0700

    Update Fix-lag.lua

--- Fix-lag.lua
+++ Fix-lag.lua
@@ -1,27 +1,6 @@
 -- Script Fix Lag By @fan_game1234 (Y-T)
 -- Đợi game load xong
-repeat task.wait() until game:IsLoaded()
-
-local Players = game:GetService("Players")
-local StarterGui = game:GetService("StarterGui")
-local player = Players.LocalPlayer
-local placeId = game.PlaceId
-
--- Bảng PlaceId -> URL file con
-local scripts = {
-    -- Blox Fruits
-    [27539155
-
-
-
-
-
-
-
-
-
-
-
+repeat task.wait()
 
 
 
