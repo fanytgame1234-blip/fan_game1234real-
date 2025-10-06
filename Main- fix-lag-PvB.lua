ce7f3d0f3e10253a11a47060564b033915b42c18
fanytgame1234-blip <fanytgame1234@gmail.com>
2025-10-06 19:12:55 +0700

    Update Main- fix-lag-PvB.lua

--- Main- fix-lag-PvB.lua
+++ Main- fix-lag-PvB.lua
@@ -4,7 +4,7 @@ repeat task.wait() until game:IsLoaded()
 
 local Pvburl = {
     "https://raw.githubusercontent.com/fanytgame1234-blip/fan_game1234real-/main/Fix-Lag-Hub-Ui.lua", 
-    "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/hide_other_player.lua", 
+   -- "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/hide_other_player.lua", 
     "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/delete_lightning_gag.lua", -- dùng lại của Gag vì tui hơi lười:) 
     "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model/main/hide_model_plant_vs_brainrot.lua", 
     "https://raw.githubusercontent.com/fanytgame1234-blip/delete_model//main/delete_sky.lua", 
@@ -18,6 +18,7 @@ for i, url in ipairs(Pvburl) do
     end)
 
 end   
+
 
 
 
